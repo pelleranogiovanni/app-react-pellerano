7af74b84385c230e7bc94c46b642b26e66848c01
pelleranogiovanni-connaxis <giovanni.pellerano@connaxis.com>
2021-09-08 22:17:29 -0300

    Add icon cart - desafio 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import './App.css';
 import "@material-tailwind/react/tailwind.css";
 import NavBar from "./components/NavBar";
+import Desafio from "./components/Desafio";
 
 const App = () => {
 
   return (
     <div className="App">
         <NavBar />      
+        {/* <Desafio /> */}
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
@@ -6,14 +7,15 @@ const NavBar = () => {
             <header className="min-h-screen bg-gray-100">
                 <nav className="flex items-center justify-between p-6 h-20 bg-white shadow-md w-full">
                     <div className="">
-                        <img className="w-12 h-12" src="./logo.png"></img>
+                        <img className="rounded-sm w-12 h-12" src="./logo.png"></img>
                     </div>
                     <ul className="">
-                    <li className="space-x-5 text-xl justify-between w-full">
+                    <li className="space-x-5 text-xl justify-between w-full flex items-center">
                         <a href="#" className=" sm:inline-block text-gray-700 mx-4 hover:text-orange-700">Home</a>
                         <a href="#" className=" sm:inline-block text-gray-700 mx-4 hover:text-orange-700">About</a>
                         <a href="#" className=" sm:inline-block text-gray-700 mx-4 hover:text-orange-700">Servics</a>
                         <a href="#" className=" sm:inline-block text-gray-700 mx-4 hover:text-orange-700">Products</a>
+                        <CartWidget />
                     </li>
                     <div className="sm:hidden space-y-1 hover:cursor-pointer">
                         <span className="w-10 h-1 bg-gray-600 rounded-full block"></span>
